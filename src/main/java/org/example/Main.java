f452e56966326dc73f9d2fa496ca360176be9f1c
Milind-dev <milinddev1011@gmail.com>
2024-04-11 21:16:25 +0530

    ioc now just change on application bean engine it reflect. now its loose coupling just change like class=org.example.Bike

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,16 @@ package org.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class  keys {
+    public void run(Engine key){
+        key.start();
+    }
+}
 public class Main {
     public static void main(String[] args) {
-      System.out.println("hello");
-      Engine engine = new Car();
-        Engine engine1 = new Bike();
-        engine.start();
-        engine1.start();
+        ApplicationContext ac = new ClassPathXmlApplicationContext("application.xml");
+        Engine key = (Engine) ac.getBean("engine");
+        new keys().run(key);
 
   }
 }
