9972d895cad9a09ca6484af85cdc7de03aea091f
Milind-dev <milinddev1011@gmail.com>
2024-04-12 10:59:06 +0530

    Bike methods property value

--- src/main/java/org/example/Bike.java
+++ src/main/java/org/example/Bike.java
@@ -2,12 +2,13 @@ package org.example;
 
 public class Bike {
    String color;
-    public Bike(){
-        System.out.print("creation of object");
-    }
-//    public void setColor(String color){
-//        this.color = color;
+//    public Bike(){
+//        System.out.print("creation of object");
 //    }
+    public void setColor(String color){
+        this.color = color;
+        System.out.println("i am setter");
+      }
     public void showColor(){
         System.out.println("bike color is = "+color);
     }
