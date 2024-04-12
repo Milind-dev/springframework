49c75e2deb150dbbe6bcf55704020d9281b9e307
Milind-dev <milinddev1011@gmail.com>
2024-04-12 11:01:53 +0530

    Bike constructor bike

--- src/main/java/org/example/Bike.java
+++ src/main/java/org/example/Bike.java
@@ -5,9 +5,13 @@ public class Bike {
 //    public Bike(){
 //        System.out.print("creation of object");
 //    }
-    public void setColor(String color){
+//    public void setColor(String color){
+//        this.color = color;
+//        System.out.println("i am setter");
+//      }
+    public  Bike(String color){
         this.color = color;
-        System.out.println("i am setter");
+        System.out.println("i am constructor");
       }
     public void showColor(){
         System.out.println("bike color is = "+color);
