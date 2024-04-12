0f8e2b6deac1cfca99ad7a5fbeb1b7d6574b8552
Milind-dev <milinddev1011@gmail.com>
2024-04-12 10:52:54 +0530

    give me a error because of setter commenting on Bike

--- src/main/java/org/example/Bike.java
+++ src/main/java/org/example/Bike.java
@@ -5,9 +5,9 @@ public class Bike {
     public Bike(){
         System.out.print("creation of object");
     }
-    public void setColor(String color){
-        this.color = color;
-    }
+//    public void setColor(String color){
+//        this.color = color;
+//    }
     public void showColor(){
         System.out.println("bike color is = "+color);
     }
