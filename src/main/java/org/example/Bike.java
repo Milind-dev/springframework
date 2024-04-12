c115fb53583f7ee9f6b7c16073a61c24d3aa45f4
Milind-dev <milinddev1011@gmail.com>
2024-04-12 10:51:40 +0530

    property value of string color in xml

--- src/main/java/org/example/Bike.java
+++ src/main/java/org/example/Bike.java
@@ -3,7 +3,7 @@ package org.example;
 public class Bike {
    String color;
     public Bike(){
-        System.out.print("Bike");
+        System.out.print("creation of object");
     }
     public void setColor(String color){
         this.color = color;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("application.xml");
+//        Bike bike = ac.getBean("bike",Bike.class);
         Bike bike = (Bike) ac.getBean("bike");
         bike.showColor(); //why bike color is null because of primitive type string color are not defined
     }
